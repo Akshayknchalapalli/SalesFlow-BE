70e192b857c241ebe6f86f016cc224449d3b8c77
Akshayknchalapalli <akshaysmart31@gmail.com>
2025-05-11 01:16:44 +0530

    updated date

--- contact-core-service/target/generated-sources/annotations/com/salesflow/contact/core/mapper/ContactMapperImpl.java
+++ contact-core-service/target/generated-sources/annotations/com/salesflow/contact/core/mapper/ContactMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-10T13:02:54+0530",
+    date = "2025-05-11T00:34:16+0530",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
