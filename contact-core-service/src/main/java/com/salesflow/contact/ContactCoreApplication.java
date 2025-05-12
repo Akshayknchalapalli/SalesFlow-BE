b61ceaf81088e9e18885e12e2945d16f545696b1
Akshayknchalapalli <akshaysmart31@gmail.com>
2025-05-12 13:38:30 +0530

    setup .env file with supabase credentials

--- contact-core-service/src/main/java/com/salesflow/contact/ContactCoreApplication.java
+++ contact-core-service/src/main/java/com/salesflow/contact/ContactCoreApplication.java
@@ -5,13 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
-
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
 @EnableKafka
 public class ContactCoreApplication {
     public static void main(String[] args) {
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+        
+        // Set the environment variables for Spring Boot
+        System.setProperty("SUPABASE_DB_URL", dotenv.get("SUPABASE_DB_URL"));
+        System.setProperty("SUPABASE_DB_PASSWORD", dotenv.get("SUPABASE_DB_PASSWORD"));
         SpringApplication.run(ContactCoreApplication.class, args);
     }
 } 
